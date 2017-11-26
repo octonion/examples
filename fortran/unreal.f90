5ebd7954f1b21881880217771fe4ca8e31a9a09f
Christopher D. Long <galizur@gmail.com>
2017-11-26 01:51:42 -0500

    Unreal 10207 example in Fortran 90.

---
+++ fortran/unreal.f90
@@ -0,0 +1,26 @@
+program unreal
+  real, external :: f
+
+  print *,f(1,1,0.5)
+  print *,f(2,2,0.5)
+  print *,f(3,3,0.5)
+  print *,f(4,4,0.5)
+  print *,f(5,5,0.5)
+  print *,1-f(10,2,0.51)
+  print *,1-f(10,3,0.51)
+  
+end program unreal
+
+recursive real function f(i,j,p) result(w)
+  integer :: i,j
+  real :: p
+  
+  if (i==0) then
+     w = 0.0
+  else if (j==0) then
+     w = 1.0
+  else
+     w = p*f(i-1,j,p)+(1-p)*f(i,j-1,p)
+  end if
+
+end function f
