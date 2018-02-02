6733e5dd24629010094d0fe4abb8c27592f78dd7
Christopher D. Long <galizur@gmail.com>
2018-02-02 02:10:12 -0500

    Corrected for-loop example.

--- dart/for-loop.dart
+++ dart/for-loop.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 main() {
-    for (var i = 0; i < 5; i++)
-        for (var j = 0; j < i + 1; j++)
-            print("*");
-        print("\n");
+  for (var i = 0; i < 5; i++) {
+    for (var j = 0; j < i + 1; j++) {
+      stdout.write("*");
+    }
+    stdout.write("\n");
+  }
 }
