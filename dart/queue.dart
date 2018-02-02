2dd807f3fa1d19e3dce5131d375db5018e8878e4
Christopher D. Long <galizur@gmail.com>
2018-02-02 01:58:50 -0500

    Dart examples.

---
+++ dart/queue.dart
@@ -0,0 +1,9 @@
+import 'dart:collection';
+
+void main() {
+  Queue<String> queue = new Queue<String>(); 
+  queue.add("a");
+  queue.add("b");
+  queue.add("c");
+  print(queue); // prints "{a, b, c}"
+}
